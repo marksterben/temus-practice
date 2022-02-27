797c867ded5fe62d157413794b3774a9c9786195
Marcellino <68014273+marksterben@users.noreply.github.com>
2022-02-28 01:53:11 +0700

    create slider component

---
+++ components/Slider.js
@@ -0,0 +1,77 @@
+import Image from "next/image";
+
+import styles from "../styles/components/Slider.module.scss";
+import slideItem from "../public/images/slider.jpg";
+import { useState } from "react";
+
+const Slider = () => {
+  return (
+    <>
+      <div className={styles.title}>
+        <p>WHY WE DIFFERENT</p>
+        <h1>THE TEMUS TOUCH:</h1>
+        <h1 className={styles.orangeText}>WHAT MAKE US UNIQUE</h1>
+      </div>
+
+      <div className={styles.slider}>
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p>We’re with You from Ideation to Implementation</p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p>We Design for Real Life</p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p></p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p></p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p></p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p></p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p></p>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          <Image src={slideItem} className={styles.image} alt="Notebook" />
+          <div className={styles.content}>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Slider;
